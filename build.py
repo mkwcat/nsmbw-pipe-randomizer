122b08e4217bdfe16294cf08bb7a2a3dd8299e7f
mkwcat <cat@mkw.re>
2024-08-14 18:35:32 -0400

    Add output for region C

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 try:
     os.mkdir("build")
@@ -10,12 +10,9 @@ try:
 except FileExistsError:
     pass
 
-os.system("Kamek\\cw\\mwcceppc.exe -I- -i Kamek\\k_stdlib -i include -pragma \"cpp1x on\" -gccext on -Cpp_exceptions off -enum int -O4,s -use_lmw_stmw on -fp hard -rostr -sdata 0 -sdata2 0 -RTTI off -DIS_GAME_VERSION_DYNAMIC -c -o build\\nsmbw-random-pipe.o src\\nsmbw-random-pipe.cpp")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\P1.bin -select-version=P1")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\P2.bin -select-version=P2")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\E1.bin -select-version=E1")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\E2.bin -select-version=E2")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\J1.bin -select-version=J1")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\J2.bin -select-version=J2")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\K.bin -select-version=K")
-os.system("Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\W.bin -select-version=W")
+subprocess.run(["Kamek\\cw\\mwcceppc.exe", "-I-", "-i", "Kamek\\k_stdlib", "-i", "include", "-pragma", "cpp1x on", "-gccext", "on", "-Cpp_exceptions", "off", "-enum", "int", "-O4,s", "-use_lmw_stmw", "on", "-fp", "hard", "-rostr", "-sdata", "0", "-sdata2", "0", "-RTTI", "off", "-DIS_GAME_VERSION_DYNAMIC", "-c", "-o", "build\\nsmbw-random-pipe.o", "src\\nsmbw-random-pipe.cpp"]).check_returncode()
+
+for region in ["P1", "P2", "E1", "E2", "J1", "J2", "K", "W", "C"]:
+    result = subprocess.run(f"Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\{region}.bin -select-version={region}".split(" "), stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        exit(f"Kamek failed linking region {region}")
