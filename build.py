d9b6dc54bbb8a9bca9c978408e9e193f59909da0
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-03-17 04:11:46 -0400

    Update build script to support multiple OSes

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import os, subprocess
+import os, sys, subprocess
 
 try:
     os.mkdir("build")
@@ -10,9 +10,13 @@ try:
 except FileExistsError:
     pass
 
-subprocess.run(["Kamek\\cw\\mwcceppc.exe", "-I-", "-i", "Kamek\\k_stdlib", "-i", "include", "-pragma", "cpp1x on", "-gccext", "on", "-Cpp_exceptions", "off", "-enum", "int", "-O4,s", "-use_lmw_stmw", "on", "-fp", "hard", "-rostr", "-sdata", "0", "-sdata2", "0", "-RTTI", "off", "-DIS_GAME_VERSION_DYNAMIC", "-c", "-o", "build\\nsmbw-random-pipe.o", "src\\nsmbw-random-pipe.cpp"]).check_returncode()
+prefix = []
+if len(sys.argv) > 1:
+    prefix = sys.argv[1:]
+
+subprocess.run(prefix + ["Kamek/cw/mwcceppc.exe", "-proc", "gekko", "-I-", "-i", "Kamek/k_stdlib", "-i", "include", "-pragma", "cpp1x on", "-gccext", "on", "-Cpp_exceptions", "off", "-enum", "int", "-O4,s", "-use_lmw_stmw", "on", "-fp", "hard", "-rostr", "-sdata", "0", "-sdata2", "0", "-RTTI", "off", "-DIS_GAME_VERSION_DYNAMIC", "-c", "-o", "build/nsmbw-random-pipe.o", "src/nsmbw-random-pipe.cpp"]).check_returncode()
 
 for region in ["P1", "P2", "E1", "E2", "J1", "J2", "K", "W", "C"]:
-    result = subprocess.run(f"Kamek\\Kamek.exe build\\nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output\\nsmbw-pipe-randomizer\\Code\\{region}.bin -select-version={region}".split(" "), stdout=subprocess.PIPE)
+    result = subprocess.run(f"Kamek/Kamek build/nsmbw-random-pipe.o -dynamic -versions=address-map.txt -externals=externals.txt -output-kamek=output/nsmbw-pipe-randomizer/Code/{region}.bin -select-version={region}".split(" "), stdout=subprocess.PIPE)
     if result.returncode != 0:
         exit(f"Kamek failed linking region {region}")
